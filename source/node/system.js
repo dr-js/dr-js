f7a9c957f6f1fa408d359da247c8f9a5067594d9
dr-js <dr@dr.city>
2017-09-27 17:56:46 +0800

    use setProcessExitListener instead of addProcessExitListener, bad naming

--- source/node/system.js
+++ source/node/system.js
@@ -11,7 +11,7 @@ const getLocalPath = (relativePath) => nodeModulePath.resolve(PATH_NODE_START_SC
 
 const PROCESS_EXIT_SIGNAL_EVENT_TYPE_LIST = [ 'SIGINT', 'SIGHUP', 'SIGQUIT', 'SIGTERM' ]
 
-const addProcessExitListener = (listener) => {
+const setProcessExitListener = (listener) => { // should only call once
   const wrappedListenerList = [
     { eventType: 'exit', wrappedListener: (code) => listener({ eventType: 'exit', code }) },
     { eventType: 'uncaughtException', wrappedListener: (error) => listener({ eventType: 'uncaughtException', error }) },
@@ -26,6 +26,10 @@ const addProcessExitListener = (listener) => {
   wrappedListenerList.forEach(({ eventType, wrappedListener }) => process.on(eventType, wrappedListener))
   return () => wrappedListenerList.forEach(({ eventType, wrappedListener }) => process.removeListener(eventType, wrappedListener))
 }
+const addProcessExitListener = (listener) => {
+  console.warn('[Deprecated Name] use setProcessExitListener')
+  return setProcessExitListener(listener)
+}
 
 const startREPL = () => nodeModuleREPL.start({
   prompt: '> ',
@@ -39,6 +43,7 @@ export {
   PATH_NODE_START_SCRIPT,
 
   getLocalPath,
+  setProcessExitListener,
   addProcessExitListener,
   startREPL
 }
