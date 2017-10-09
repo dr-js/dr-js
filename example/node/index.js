4cc48ea7b00a1860bc7450c0dbe0565a50f0b58f
dr-js <dr@dr.run>
2017-10-09 12:20:16 +0800

    dr-js@0.3.0
    deprecated `addProcessExitListener`

--- example/node/index.js
+++ example/node/index.js
@@ -1,15 +1,12 @@
 const Dr = require('../Dr.node')
 
-console.log(Object.keys(Dr))
-console.log(Object.keys(Dr.Node))
+// console.log(Object.keys(Dr))
+// console.log(Object.keys(Dr.Node))
 
 const { runCommand } = Dr.Node.Module
 
-runCommand('dir /w').then(console.log)
+runCommand(process.platform === 'win32' ? 'dir' : 'ls -l')
+  .then(console.log, console.error)
 
-// const replServer = Dr.Node.System.startREPL()
-// Object.defineProperty(
-//   replServer.context,
-//   'Dr',
-//   { configurable: false, enumerable: true, value: Dr }
-// )
+const replServer = Dr.Node.System.startREPL()
+replServer.context.Dr = Dr // set global
