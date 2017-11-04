36988188f79b64fcd0a637b9fec31a2fc8ae3e1b
dr-js <dr@dr.city>
2017-11-04 18:40:13 +0800

    better SafeWrite

--- source/node/module/SafeWrite.js
+++ source/node/module/SafeWrite.js
@@ -1,22 +1,23 @@
 import nodeModuleFs from 'fs'
 
-// TODO: almost safe to use, check test for exceptions
-
 // async write normally, sync write on emergency
-const createSafeWriteStream = ({ pathOutputFile, onError = DEFAULT_ON_ERROR, flags = 'a', mode = 0o666 }) => {
-  let fileDescriptor = nodeModuleFs.openSync(pathOutputFile, flags, mode)
+const createSafeWriteStream = ({ pathOutputFile, onError = DEFAULT_ON_ERROR, flag = 'a', mode = 0o666 }) => {
+  let fileDescriptor = nodeModuleFs.openSync(pathOutputFile, flag, mode)
   let writingQueue = []
   let pendingQueue = []
+  const onWriteError = (error) => error ? onError(error) : (writingQueue.length = 0)
+  const doWrite = (string) => fileDescriptor && nodeModuleFs.write(fileDescriptor, string, onWriteError)
   const write = (writeString) => {
     if (!writeString) return
     pendingQueue.push(writeString)
     if (writingQueue.length !== 0) return
     [ writingQueue, pendingQueue ] = [ pendingQueue, [] ]
-    nodeModuleFs.write(fileDescriptor, writingQueue.join(''), (error) => (fileDescriptor && error) ? onError(error) : (writingQueue.length = 0))
+    process.nextTick(doWrite, writingQueue.join(''))
   }
   const end = () => {
-    pendingQueue.length !== 0 && nodeModuleFs.writeSync(fileDescriptor, pendingQueue.join('')) // TODO: TEST: no need to rewrite writingQueue
-    process.nextTick((fileDescriptor) => nodeModuleFs.closeSync(fileDescriptor), fileDescriptor)
+    const remainQueue = [ ...writingQueue, ...pendingQueue ]
+    remainQueue.length !== 0 && nodeModuleFs.writeSync(fileDescriptor, remainQueue.join(''))
+    nodeModuleFs.closeSync(fileDescriptor)
     fileDescriptor = null
     pendingQueue.length = writingQueue.length = 0
   }
