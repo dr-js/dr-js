34ce5228c833102a8a58191c93955ff7c88d96c6
dr-js <dr@dr.city>
2017-12-12 12:42:49 +0800

    dr-js@0.9.0-dev.0
    break change:
    - use `Common.Time.getTimestamp` instead of `getTimeStamp`

--- source/common/time.js
+++ source/common/time.js
@@ -1,49 +1,47 @@
 import { global } from 'source/env'
 
 const getClock = () => {
-  const { performance, process } = global
   try {
-    const clock = performance.now // milliseconds
+    const clock = global.performance.now // msec
     const time = clock()
     if (time <= clock()) return clock
   } catch (error) { }
   try {
-    const clock = performance.now.bind(performance)
+    const clock = global.performance.now.bind(global.performance)
     const time = clock()
     if (time <= clock()) return clock
   } catch (error) { }
   try {
     const clock = () => {
-      const [ seconds, nanoseconds ] = process.hrtime()
+      const [ seconds, nanoseconds ] = global.process.hrtime()
       return seconds * 1000 + nanoseconds * 0.000001
     }
     const time = clock()
     if (time <= clock()) return clock
   } catch (error) { }
-  return Date.now
+  return Date.now // last fallback
 }
 
-const clock = getClock() // return running time in milliseconds
 const CLOCK_PER_SECOND = 1000
 const CLOCK_TO_SECOND = 1 / CLOCK_PER_SECOND
 const TIMESTAMP_START = Math.floor(Date.now() * CLOCK_TO_SECOND) // UTC
+
+const clock = getClock() // return running time in milliseconds
 const now = () => (Date.now() * CLOCK_TO_SECOND - TIMESTAMP_START) // return running time in seconds
-const getTimeStamp = () => Math.floor(Date.now() * CLOCK_TO_SECOND) // UTC
+const getTimestamp = () => Math.floor(Date.now() * CLOCK_TO_SECOND) // UTC
 
 // Usage:
 // const getData = async () => {
 //   await setTimeoutAsync(500)
 //   return 'DATA'
 // }
-// NOTE: not cancellable
 const setTimeoutAsync = (wait = 0) => new Promise((resolve) => setTimeout(resolve, wait))
 
 // Usage:
 // Promise.resolve('DATA')
-//   .then(setTimeoutPromise(500))
+//   .then(setTimeoutPromise(500)) // will pass data through
 //   .then((result) => {}) // result === 'DATA'
-// NOTE: not cancellable
-const setTimeoutPromise = (wait = 0) => (result) => new Promise((resolve) => setTimeout(() => resolve(result), wait))
+const setTimeoutPromise = (wait = 0) => (data) => new Promise((resolve) => setTimeout(() => resolve(data), wait))
 
 const onNextProperUpdate = global.requestAnimationFrame
   ? (callback) => {
@@ -61,7 +59,7 @@ export {
   TIMESTAMP_START,
   clock,
   now,
-  getTimeStamp,
+  getTimestamp,
   setTimeoutAsync,
   setTimeoutPromise,
   onNextProperUpdate
--- source/common/time.test.js
+++ source/common/time.test.js
@@ -1,8 +1,16 @@
-import { onNextProperUpdate } from './time'
+import nodeModuleAssert from 'assert'
+import {
+  clock,
+  onNextProperUpdate
+} from './time'
 
 const { describe, it } = global
 
 describe('Common.Time', () => {
+  it('clock() should get msec precision', () => {
+    nodeModuleAssert.notEqual(clock() - clock(), 0)
+  })
+
   it('onNextProperUpdate() should invoke under 500 msec', () => new Promise((resolve, reject) => {
     onNextProperUpdate(resolve)
     setTimeout(() => reject(new Error('onNextProperUpdate did not call after 500msec')), 500)
