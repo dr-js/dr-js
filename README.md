761bf88858339ca66bc7f9ed76e1ed175174366f
dr-js <dr@dr.run>
2017-12-28 21:53:15 +0800

    dr-js@0.9.2-dev.0
    change:
    - fix `createHistoryStateStore` popstate
    - fix `createStateStoreEnhanced`dispatchingState

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # dr-js
 
-[![i:npm:dr-js]][l:npm:dr-js]
-[![i:yarn:dr-js]][l:yarn:dr-js]
-[![i:status:dr-js]][l:status:dr-js]
-[![i:code-style:standard-js]][l:code-style:standard-js]
+[![i:npm]][l:npm]
+[![i:yarn]][l:yarn]
+[![i:status]][l:status]
+[![i:code-style]][l:code-style]
 [![i:es-2017]][l:es-2017]
 
 A collection of strange functions
 
-[i:npm:dr-js]: https://img.shields.io/npm/v/dr-js.svg
-[l:npm:dr-js]: https://www.npmjs.com/package/dr-js
-[i:yarn:dr-js]: https://img.shields.io/badge/yarn-dr--js-blue.svg
-[l:yarn:dr-js]: https://yarn.pm/dr-js
-[i:status:dr-js]: https://img.shields.io/travis/dr-js/dr-js/master.svg
-[l:status:dr-js]: https://travis-ci.org/dr-js/dr-js
-[i:code-style:standard-js]: https://img.shields.io/badge/code_style-standard-brightgreen.svg
-[l:code-style:standard-js]: https://standardjs.com
+[i:npm]: https://img.shields.io/npm/v/dr-js.svg
+[l:npm]: https://www.npmjs.com/package/dr-js
+[i:yarn]: https://img.shields.io/badge/yarn-dr--js-blue.svg
+[l:yarn]: https://yarn.pm/dr-js
+[i:status]: https://img.shields.io/travis/dr-js/dr-js/master.svg
+[l:status]: https://travis-ci.org/dr-js/dr-js
+[i:code-style]: https://img.shields.io/badge/code_style-standard-brightgreen.svg
+[l:code-style]: https://standardjs.com
 [i:es-2017]: https://img.shields.io/badge/mostly-ES2017-blue.svg
 [l:es-2017]: http://node.green
--- source/browser/module/HistoryStateStore.js
+++ source/browser/module/HistoryStateStore.js
@@ -1,34 +1,33 @@
-// special store, state is href string, and no getState method
+// special store, state is href string
 
 const createHistoryStateStore = (state = window.location.href) => {
   const getState = () => {
     if (listenerSet.size === 0) throw new Error('[createHistoryStateStore] should not getState before subscribe')
     return state
   }
-  const setState = (nextState) => {
+  const setState = (nextState, shouldPushState) => {
+    if (typeof (nextState) !== 'string') throw new Error(`[createHistoryStateStore] unexpected non-string href: ${nextState}`)
     if (nextState === state) return state
-    if (__DEV__ && typeof (nextState) !== 'string') throw new Error(`[createHistoryStateStore] unexpected non-string href: ${nextState}`)
     const prevState = state
     state = nextState
-    window.history.pushState(null, '', nextState)
+    shouldPushState !== 'skip' && window.history.pushState(null, '', state)
     listenerSet.forEach((listener) => listener(state, prevState))
     return state
   }
-  const listenerHistoryState = () => { setState(window.location.href) }
+  const listenerPopState = () => { setState(window.location.href, 'skip') }
+  const listenerHashChange = () => { setState(window.location.href) }
   const listenerSet = new Set()
   const subscribe = (listener) => {
-    if (listenerSet.size === 0) {
-      window.addEventListener('popstate', listenerHistoryState)
-      window.addEventListener('hashchange', listenerHistoryState)
-    }
     listenerSet.add(listener)
+    if (listenerSet.size !== 0) return
+    window.addEventListener('popstate', listenerPopState)
+    window.addEventListener('hashchange', listenerHashChange)
   }
   const unsubscribe = (listener) => {
     listenerSet.delete(listener)
-    if (listenerSet.size === 0) {
-      window.removeEventListener('popstate', listenerHistoryState)
-      window.removeEventListener('hashchange', listenerHistoryState)
-    }
+    if (listenerSet.size !== 0) return
+    window.removeEventListener('popstate', listenerPopState)
+    window.removeEventListener('hashchange', listenerHashChange)
   }
   return { getState, setState, subscribe, unsubscribe }
 }
