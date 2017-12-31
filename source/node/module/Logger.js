323f28e51bd7b2b7db07261621baabbd36ba4d8c
dr-js <dr@dr.city>
2018-01-01 01:57:54 +0800

    dr-js@0.9.2-dev.2
    deprecate: 
    - 'Node.Server.Responder.Statistic'
    
    changes:
    - change argument for `Node.Module.Logger`

--- source/node/module/Logger.js
+++ source/node/module/Logger.js
@@ -4,25 +4,29 @@ import { getRandomId } from 'source/common/math'
 import { createDirectory } from 'source/node/file'
 import { createSafeWriteStream } from './SafeWrite'
 
-const createSimpleLogger = ({ pathOutputFile, onError, queueLengthThreshold }) => createLogQueue({
-  outputStream: createSafeWriteStream({ pathOutputFile, onError }),
+const createSimpleLogger = ({ pathOutputFile, queueLengthThreshold, flag, mode, onError }) => createLogQueue({
+  outputStream: createSafeWriteStream({ pathOutputFile, flag, mode, onError }),
   queueLengthThreshold
 })
 
 const FILE_SPLIT_INTERVAL = 24 * 60 * 60 * 1000 // 24hour
+const DEFAULT_GET_LOG_FILE_NAME = (prefixLogFile) => `${getRandomId(prefixLogFile)}.log`
 
 const createLogger = async ({
   pathLogDirectory,
   prefixLogFile = '',
+  getLogFileName = DEFAULT_GET_LOG_FILE_NAME,
   queueLengthThreshold,
   fileSplitInterval = FILE_SPLIT_INTERVAL,
+  flag,
+  mode,
   onError
 }) => {
   await createDirectory(pathLogDirectory)
   let logger = null
   const splitLogFile = () => {
     logger && logger.end()
-    logger = createSimpleLogger({ pathOutputFile: nodeModulePath.join(pathLogDirectory, `${prefixLogFile}${getRandomId()}.log`), queueLengthThreshold, onError })
+    logger = createSimpleLogger({ pathOutputFile: nodeModulePath.join(pathLogDirectory, getLogFileName(prefixLogFile)), queueLengthThreshold, flag, mode, onError })
   }
   splitLogFile()
   let intervalToken = setInterval(splitLogFile, fileSplitInterval)
