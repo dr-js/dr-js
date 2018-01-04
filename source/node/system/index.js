f265b23527d15b9aab58b98e48b6c1355c4f38e3
dr-js <dr@dr.city>
2018-01-04 15:39:07 +0800

    dr-js@0.9.3
    change:
    - fix `PATH_NODE_START_SCRIPT` from `Node.System` throws if `process.argv[ 1 ]` is `undefined`
    - add `Common.Module.StateSchema`
    - more stable auto port for `bin -m server-serve-static`

--- source/node/system/index.js
+++ source/node/system/index.js
@@ -6,7 +6,7 @@ export { getNetworkIPv4AddressList } from './NetworkAddress'
 export { startREPL } from './REPL'
 
 const PATH_NODE_EXE = global.process.argv[ 0 ]
-const PATH_NODE_START_SCRIPT = nodeModulePath.resolve(global.process.cwd(), nodeModulePath.dirname(global.process.argv[ 1 ]))
+const PATH_NODE_START_SCRIPT = nodeModulePath.resolve(global.process.cwd(), nodeModulePath.dirname(global.process.argv[ 1 ] || ''))
 const getLocalPath = (relativePath) => nodeModulePath.resolve(PATH_NODE_START_SCRIPT, relativePath)
 
 export { PATH_NODE_EXE, PATH_NODE_START_SCRIPT, getLocalPath }
