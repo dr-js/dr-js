60b49bd04867cede5cb40adfa695235294b3bf9d
dr-js <dr@dr.city>
2018-01-07 14:30:09 +0800

    dr-js@0.9.4-dev.0
    change:
    - add `Common.Immutable.StateStore.makeReduxLikeListener`

--- source/common/immutable/index.js
+++ source/common/immutable/index.js
@@ -1,5 +1,4 @@
 import * as ImmutableOperation from './ImmutableOperation'
 export { immutableTransformCache, createImmutableTransformCacheWithInfo } from './__utils__'
-export { createStateStore, createStateStoreLite, createStateStoreEnhanced } from './StateStore'
-
+export { createStateStore, createStateStoreLite, createStateStoreEnhanced, makeReduxLikeListener } from './StateStore'
 export { ImmutableOperation }
