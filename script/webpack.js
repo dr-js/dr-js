5506c2a7ba7a47841b71b821924ee437926319f7
dr-js <dr@dr.run>
2018-03-02 01:16:07 +0800

    dr-js@0.12.0-dev.2
    change:
    - merge `exportTree.md` and `exportPath.md` to `EXPORT_INFO.md`
    - move common script to `dev-dep-tool`

--- script/webpack.js
+++ script/webpack.js
@@ -1,7 +1,7 @@
 import { resolve as resolvePath } from 'path'
 import { loadFlag, checkFlag, runMain } from 'dev-dep-tool/library/__utils__'
 import { getLogger } from 'dev-dep-tool/library/logger'
-import { binary, time } from 'source/common/format'
+import { compileWithWebpack } from 'dev-dep-tool/library/webpack'
 import webpack from 'webpack'
 
 const PATH_ROOT = resolvePath(__dirname, '..')
@@ -15,37 +15,19 @@ const BABEL_OPTIONS = {
   plugins: [ [ '@babel/proposal-class-properties' ], [ '@babel/proposal-object-rest-spread', { useBuiltIns: true } ] ]
 }
 
-const compileWithWebpack = async ({ MODE, logger }) => {
-  const IS_PRODUCTION = MODE === 'production'
+runMain(async (logger) => {
+  const MODE = checkFlag(loadFlag([ 'development', 'production' ]), [ 'development', 'production' ], 'production')
+  const isWatch = Boolean(checkFlag(loadFlag([ 'watch' ]), [ 'watch' ]))
 
-  const compiler = webpack({
+  const config = {
     mode: MODE,
     output: { path: fromOutput('library'), filename: '[name].js', library: 'Dr', libraryTarget: 'umd' },
     entry: { 'Dr.browser': 'source/Dr.browser' },
     resolve: { alias: { source: fromRoot('source') } },
     module: { rules: [ { test: /\.js$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: BABEL_OPTIONS } } ] },
-    plugins: [ new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(MODE), __DEV__: !IS_PRODUCTION }) ]
-  })
+    plugins: [ new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(MODE), __DEV__: MODE !== 'production' }) ]
+  }
 
-  return new Promise((resolve, reject) => compiler.run((error, stats) => {
-    if (error) return reject(error)
-
-    stats.hasErrors() && stats.toJson().errors.forEach((message) => console.error(message))
-    stats.hasWarnings() && stats.toJson().warnings.forEach((message) => console.warn(message))
-    if (stats.hasErrors()) return reject(new Error('webpack stats Error'))
-
-    Object.entries(stats.compilation.assets).forEach(([ name, sourceInfo ]) => {
-      logger.log(`output: ${name} [${binary(sourceInfo.size())}B] emitted: ${sourceInfo.emitted}`)
-    })
-    logger.log(`time: ${time(stats.endTime - stats.startTime)}`)
-
-    resolve(stats)
-  }))
-}
-
-runMain(async (logger) => {
-  const MODE = checkFlag(loadFlag([ 'development', 'production' ]), [ 'development', 'production' ], 'production')
-
-  logger.padLog(`compile with webpack mode: ${MODE}`)
-  await compileWithWebpack({ MODE, logger })
+  logger.padLog(`compile with webpack mode: ${MODE}, isWatch: ${isWatch}`)
+  await compileWithWebpack({ config, isWatch, logger })
 }, getLogger(`webpack`))
