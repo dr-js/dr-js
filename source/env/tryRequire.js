438522a2535461e11b79470618d0f5cf247453d1
dr-js <dr@dr.run>
2018-07-16 18:38:43 +0800

    dr-js@0.16.0-dev.8

--- source/env/tryRequire.js
+++ source/env/tryRequire.js
@@ -1,11 +1,23 @@
+import { global } from './global'
+
 // TODO: will cause webpack warning `Critical dependency: the request of a dependency is an expression`, but whatever
 // TODO: put in separate folder here so webpack will not assume whole package is required
+// check: https://github.com/webpack/webpack/issues/196
+// check: https://github.com/sindresorhus/got/commit/a4ce0a738b25a02c2b6b50a4a52e94e5974f2f8b
 // - - If the module source contains a require that cannot be statically analyzed, the context is the current directory.
 // - - In this case a Critical dependencies warning is emitted.
-const tryRequire = (name = '') => {
-  try {
-    return require(name)
-  } catch (error) { __DEV__ && console.log(`[TRY_REQUIRE] failed for ${name}`, error) }
+
+const getTryRequire = () => {
+  const REQUIRE = (global.module && global.module.require) ||
+    (module && module.require) ||
+    global.require // do not use local context require directly
+  return (name = '') => {
+    try {
+      return REQUIRE(name)
+    } catch (error) { __DEV__ && console.log(`[tryRequire] failed for ${name}`, error) }
+  }
 }
+
+const tryRequire = getTryRequire()
 
 export { tryRequire }
