37bcb71d8c93ff7e6111a6ffc34b26ce967da1ba
dr-js <dr@dr.run>
2021-08-23 12:30:07 +0800

    BUMP: ci: config

--- .github/ci-patch.js
+++ .github/ci-patch.js
@@ -1,11 +1,12 @@
-const { runMain, commonInfoPatchCombo } = require('@dr-js/dev/library/main.js')
+const { runKit } = require('@dr-js/core/library/node/kit.js')
+const { runInfoPatchCombo } = require('@dr-js/dev/library/ci.js')
 
-runMain(async (logger) => {
-  const { RUN, fromHome, config: { COMMAND_SUDO_NPM } } = commonInfoPatchCombo(logger)
+runKit(async (kit) => {
+  runInfoPatchCombo(kit)
 
-  logger.padLog('Patch npm cache path') // set cache path to `~/.npm/` for all platform (only win32 for now)
-  RUN([ ...`${COMMAND_SUDO_NPM} config --global set cache`.split(' '), fromHome('.npm/') ])
+  kit.padLog('Patch npm cache path') // set cache path to `~/.npm/` for all platform (only win32 for now)
+  kit.RUN_SUDO_NPM([ 'config', '--global', 'set', 'cache', kit.fromHome('.npm/') ])
 
-  logger.padLog('Patch install "@dr-js/dev" globally')
-  RUN(`${COMMAND_SUDO_NPM} install --global @dr-js/dev@0.4`)
-}, 'ci-patch')
+  kit.padLog('Patch install "@dr-js/dev" globally')
+  kit.RUN_SUDO_NPM('install --global @dr-js/dev@0.4')
+}, { title: 'ci-patch' })
