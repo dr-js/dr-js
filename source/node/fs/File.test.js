433a6ca4467832ee5454e62da8d18a958131e9a0
dr-js <dr@dr.run>
2021-09-14 15:38:51 +0800

    FIX: node: type convert for `*ArrayBuffer`

--- source/node/fs/File.test.js
+++ source/node/fs/File.test.js
@@ -1,11 +1,13 @@
 import { resolve } from 'path'
-import { stringifyEqual } from 'source/common/verify.js'
+import { stringifyEqual, truthy } from 'source/common/verify.js'
 import { withRetryAsync } from 'source/common/function.js'
+import { isEqualArrayBuffer } from 'source/common/data/ArrayBuffer.js'
 import { deleteDirectory, resetDirectory } from './Directory.js'
 
 import {
   writeText,
-  readJSONAlike, readText
+  writeArrayBuffer, readArrayBuffer,
+  readJSONAlikeSync, readText
 } from './File.js'
 
 const { describe, it, before, after, info = console.log } = globalThis
@@ -21,16 +23,22 @@ after(async () => {
 })
 
 describe('Node.Fs.File', () => {
-  it('readJSONAlike()', async () => {
+  it('writeArrayBuffer()', async () => {
+    const SOURCE_FILE = resolve(TEST_ROOT, './array-buffer.file')
+    const SOURCE_ARRAY_BUFFER = await readArrayBuffer(resolve(__filename))
+    await writeArrayBuffer(SOURCE_FILE, SOURCE_ARRAY_BUFFER)
+    truthy(isEqualArrayBuffer(await readArrayBuffer(SOURCE_FILE), SOURCE_ARRAY_BUFFER))
+  })
+  it('readJSONAlikeSync()', async () => {
     const SOURCE_FILE = resolve(TEST_ROOT, './json-alike.js')
     await writeText(SOURCE_FILE, 'module.exports = { a: 1 }')
     info(await readText(SOURCE_FILE))
-    stringifyEqual(readJSONAlike(SOURCE_FILE), { a: 1 })
+    stringifyEqual(readJSONAlikeSync(SOURCE_FILE), { a: 1 })
 
     await withRetryAsync(async () => { // TODO: flaky test with babel transpile
       await writeText(SOURCE_FILE, 'module.exports = { b: 2 }')
       info(await readText(SOURCE_FILE))
-      stringifyEqual(readJSONAlike(SOURCE_FILE), { b: 2 })
+      stringifyEqual(readJSONAlikeSync(SOURCE_FILE), { b: 2 })
     }, 6)
   })
 })
