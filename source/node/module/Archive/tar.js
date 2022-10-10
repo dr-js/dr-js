c5e597373322c16bf074071b91e865e0f4ed95a9
dr-js <dr@dr.run>
2022-10-10 02:28:02 +0000

    FIX: node: probe tar under win32 git-bash

--- source/node/module/Archive/tar.js
+++ source/node/module/Archive/tar.js
@@ -1,14 +1,14 @@
 import { resolve } from 'node:path'
 import { probeSync, createArgListPack } from '../function.js'
 
-// $ tar --version
+// $ tar --version # linux
 //   tar (GNU tar) 1.28
-// > tar --version
+// > tar --version # win32 & darwin
 //   bsdtar 3.3.2 - libarchive 3.3.2 zlib/1.2.5.f-ipp
 const { getArgs, setArgs, check, verify } = createArgListPack(
-  () => probeSync([ 'tar', '--version' ], 'tar')
-    ? [ 'tar' ]
-    : undefined,
+  () => process.platform === 'win32' && probeSync([ 'C:\\Windows\\System32\\tar.exe', '--version' ], 'bsdtar') ? [ 'C:\\Windows\\System32\\tar.exe' ] // NOTE: for win32 git-bash: use system tar, as the bundled tar expect path like `/c/Users/` not `C:\Users\`
+    : probeSync([ 'tar', '--version' ], 'tar') ? [ 'tar' ]
+      : undefined,
   'expect "tar" in PATH'
 )
 
