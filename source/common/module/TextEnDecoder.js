9078ada287cb6fb8c8f6b7d36477aed23f26703f
dr-js <dr@dr.run>
2023-04-19 09:37:13 +0000

    CHG: code sort

--- source/common/module/TextEnDecoder.js
+++ source/common/module/TextEnDecoder.js
@@ -1,6 +1,5 @@
 // https://developer.mozilla.org/docs/Web/API/TextEncoder
-// should exist in Chrome@38, Edge@79, Safari@10.1, Firefox@20, not IE
-// and nodejs@12
+// should exist in Chrome@38, Edge@79, Safari@10.1, Firefox@20 and nodejs@12, not IE
 const tryTextEnDecoder = () => {
   try { // browser & node
     const { TextEncoder, TextDecoder } = globalThis
@@ -13,14 +12,19 @@ const tryTextEnDecoder = () => {
   return [ () => {}, () => {} ] // no fallback, will error on 'new TextEncoder'
 }
 
-const [ TextEncoder, TextDecoder ] = tryTextEnDecoder()
+const [ _TextEncoder, _TextDecoder ] = tryTextEnDecoder() // TODO: DEPRECATED: direct use
 
 /** @type { (v: string) => Uint8Array } */
 const encodeUTF8 = (string) => new TextEncoder().encode(string)
 /** @type { (v: ArrayBuffer | Uint8Array | DataView) => string } */
 const decodeUTF8 = (buffer) => new TextDecoder().decode(buffer)
 
+/** @deprecated */ const __TextEncoder = _TextEncoder
+/** @deprecated */ const __TextDecoder = _TextDecoder
+
 export {
-  TextEncoder, TextDecoder,
-  encodeUTF8, decodeUTF8
+  encodeUTF8, decodeUTF8,
+
+  __TextEncoder as TextEncoder, // TODO: DEPRECATED
+  __TextDecoder as TextDecoder // TODO: DEPRECATED
 }
