38a768f9c1721e54b45df93f8ecf7d38ef7af9eb
dr-js <dr@dr.run>
2023-05-17 08:38:56 +0000

    CHG: smaller `BASIC_EXTENSION_MAP` data

--- source/common/module/MIME.js
+++ source/common/module/MIME.js
@@ -1,63 +1,23 @@
-const BASIC_MIME_DATA_LIST = [ // https://developer.mozilla.org/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
-  'application/gzip;gz',
-  'application/json',
-  'application/pdf',
-  'application/wasm',
-  'application/xml',
-  'application/x-7z-compressed;7z',
-  'application/x-tar;tar',
-  'application/zip',
-
-  // 'application/msword;doc',
-  // 'application/vnd.ms-excel;xls',
-  // 'application/vnd.ms-powerpoint;ppt',
-  // 'application/vnd.openxmlformats-officedocument.wordprocessingml.document;docx',
-  // 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;xlsx',
-  // 'application/vnd.openxmlformats-officedocument.presentationml.presentation;pptx',
-
-  'audio/midi;mid',
-  'audio/mpeg;mp3',
-  'audio/ogg',
-  'audio/wav',
-  'audio/webm;weba',
-  'audio/x-flac;flac',
-
-  'font/ttf',
-  'font/otf',
-  'font/woff',
-  'font/woff2',
-
-  'image/bmp',
-  'image/gif',
-  'image/jpeg;jpg',
-  'image/png',
-  'image/svg+xml;svg',
-  'image/vnd.microsoft.icon;ico',
-  'image/webp',
-
-  'text/css',
-  'text/csv',
-  'text/html;htm',
-  'text/javascript;js;mjs;cjs', // 'application/javascript' is obsolete: https://stackoverflow.com/questions/876561/when-serving-javascript-files-is-it-better-to-use-the-application-javascript-or // TODO: HACK: according to SPEC '.cjs' should use 'application/node': https://github.com/nodejs/modules/issues/146
-  'text/plain;txt;text;conf;log;ini',
-  'text/rtf',
-  'text/xml',
-
-  'video/mp4;mp4v;mpg4',
-  'video/mpeg;mpg',
-  'video/webm;mkv', // NOTE: should overwrite `webm` from above 'audio/webm' // TODO: HACK: allow inline playback for some '.mkv': https://superuser.com/questions/1281836/what-does-matroska-have-which-webm-doesnt-that-made-the-differentiation-necess
-  'video/x-flv;flv',
-  'video/x-ms-wmv;wmv',
-  'video/x-msvideo;avi'
-]
-
+// https://developer.mozilla.org/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
 const DEFAULT_MIME = 'application/octet-stream'
 
-const BASIC_EXTENSION_MAP = BASIC_MIME_DATA_LIST.reduce((o, mimeData) => {
-  const [ mime ] = mimeData.split(';')
-  mimeData.split('/')[ 1 ].split(';').forEach((extension) => (o[ extension ] = mime))
-  return o
-}, {})
+// NOTE: 'application/javascript' is obsolete: https://stackoverflow.com/questions/876561/when-serving-javascript-files-is-it-better-to-use-the-application-javascript-or // TODO: HACK: according to SPEC '.cjs' should use 'application/node': https://github.com/nodejs/modules/issues/146
+// NOTE: `video/webm` should overwrite above 'audio/webm' // TODO: HACK: allow inline playback for some '.mkv': https://superuser.com/questions/1281836/what-does-matroska-have-which-webm-doesnt-that-made-the-differentiation-necess
+const BASIC_EXTENSION_MAP = `application|gzip;gz|json|pdf|wasm|xml|x-7z-compressed;7z|x-tar;tar|zip
+audio|midi;mid|mpeg;mp3|ogg|wav|webm;weba|x-flac;flac
+font|ttf|otf|woff|woff2
+image|bmp|gif|jpeg;jpg|png|svg+xml;svg|vnd.microsoft.icon;ico|webp
+text|css|csv|html;htm|javascript;js;mjs;cjs|plain;txt;text;conf;log;ini|rtf|xml
+video|mp4;mp4v;mpg4|mpeg;mpg|webm;mkv|x-flv;flv|x-ms-wmv;wmv|x-msvideo;avi`
+  .split('\n')
+  .reduce((o, mimeData) => {
+    const [ type, ...subDataList ] = mimeData.split('|')
+    subDataList.forEach((subData) => {
+      const extList = subData.split(';')
+      extList.forEach((ext) => { o[ ext ] = `${type}/${extList[ 0 ]}` })
+    })
+    return o
+  }, {})
 
 const REGEXP_EXTENSION = /\.(\w+)$/
 const getMIMETypeFromFileName = (fileName) => {
