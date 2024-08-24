7311cced3c1a38f70e5b82961f7e1b6d5692f1df
dr-js <dr@dr.run>
2024-08-24 17:01:46 +0000

    DOC: common: mark `objectSortKey` limitation

--- source/common/mutable/Object.js
+++ source/common/mutable/Object.js
@@ -12,6 +12,16 @@ const objectMergeDeep = (object, mergeObject) => {
   return object
 }
 
+// TODO: NOTE: not all object key is "sortable" in ES2020, there's hard order for:
+//   - Numeric array keys
+//   - non-Symbol keys, in insertion order
+//   - Symbol keys, in insertion order
+//   so
+//   { '1a': 1, '01': 2, '24': 3 } will be sort to
+//   { '24': 3, '01': 2, '1a': 1 } instead of
+//   { '01': 2, '24': 3, '1a': 1 }
+//   as '24' is Numeric array key
+//   check: https://stackoverflow.com/questions/30076219/does-es6-introduce-a-well-defined-order-of-enumeration-for-object-properties/60121411#60121411
 const objectSortKey = (object, compareKeyFunc = compareStringLocale) => {
   Object.keys(object).sort(compareKeyFunc).forEach((key) => {
     const value = object[ key ]
